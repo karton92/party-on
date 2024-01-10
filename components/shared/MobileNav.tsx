b32fe91f47b15dbd11d1e0b149aab2b2c251bf8e
karton92 <michalaugustyn92@gmail.com>
2024-01-10 14:19:27 +0100

    Initial commit

---
+++ components/shared/MobileNav.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
+import NavItems from "./NavItems";
+
+const MobileNav = () => {
+  return (
+    <nav className="md:hidden ">
+      <Sheet>
+        <SheetTrigger className="align-middle">
+          <Image src="/assets/icons/menu.svg" alt="menu" width={24} height={24} />
+        </SheetTrigger>
+        <SheetContent className="bg-white flex flex-col gap-6 md:hidden">
+          <Image src="/assets/images/logo.svg" alt="logo" width={128} height={38} />
+          <Separator />
+          <NavItems />
+        </SheetContent>
+      </Sheet>
+    </nav>
+  );
+};
+
+export default MobileNav;
